6e5bd87792024751408daeb227b42471e53c57a9
admin <noahcoder11@gmail.com>
2023-01-04 13:14:53 -0800

    test

--- about/main.js
+++
@@ -1,7 +0,0 @@
-function ref(identifier){
-    return document.querySelector(identifier)
-}
-
-let h1 = ref("#about")
-
-h1.textContent = "about page at " + window.location
--- main.js
+++
@@ -1,7 +0,0 @@
-function ref(identifier){
-    return document.querySelector(identifier)
-}
-
-let h1 = ref("#home")
-
-h1.textContent = "homepage at " + window.location
