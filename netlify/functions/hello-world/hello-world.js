94431e2934f58b84c2f9b029f59e802f42ef0bd3
admin <noahcoder11@gmail.com>
2023-01-04 09:16:17 -0800

    more testing

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,5 +1,6 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
+  console.log(event)
   try {
     const subject = event.queryStringParameters.name || 'World'
     return {
