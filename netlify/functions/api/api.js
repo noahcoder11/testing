b945523ee5b836031ce8b1b47917c0b138bd4e3d
admin <noahcoder11@gmail.com>
2023-01-04 22:14:20 -0800

    import change

--- netlify/functions/api/api.js
+++ netlify/functions/api/api.js
@@ -1,6 +1,7 @@
 // Imports
+import fetch from "node-fetch"
+
 const serverless = require("serverless-http")
-const fetch = require('node-fetch')
 const jwt = require("jsonwebtoken")
 const express = require("express")
 const dotenv = require("dotenv")
