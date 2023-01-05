cff2e9c11f7149c0fe02a278d5ebef5194396270
admin <noahcoder11@gmail.com>
2023-01-05 13:51:47 -0800

    test

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ var a2 = document.querySelector("#_testdata")
 
 a.href = window.location + "/.netlify/functions/api"
 a.href = window.location + "/.netlify/functions/api/testdata"
+
+console.log(window.location)
