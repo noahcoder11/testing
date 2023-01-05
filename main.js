b3f7f4610899c8ee96e1deaef408533bf20c755f
admin <noahcoder11@gmail.com>
2023-01-05 13:49:49 -0800

    test

---
+++ main.js
@@ -0,0 +1,5 @@
+var a = document.querySelector("#/")
+var a2 = document.querySelector("#/testdata")
+
+a.href = window.location + "/.netlify/functions/api"
+a.href = window.location + "/.netlify/functions/api/testdata"
