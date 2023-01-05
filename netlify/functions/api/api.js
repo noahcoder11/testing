7adafdce4cb2a31a20c4fe20c0a524f39dac1f8e
admin <noahcoder11@gmail.com>
2023-01-05 13:54:21 -0800

    test

--- netlify/functions/api/api.js
+++ netlify/functions/api/api.js
@@ -28,7 +28,7 @@ router.get("/", (req, res) => {
 })
 
 router.get("/testdata", (req, res) => {
-  https.get(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes`, (resp) => {
+  https.get(`https://www.googleapis.com/books/v1/volumes?q=mistborn`, (resp) => {
     let data = ''
 
     resp.on('data', (chunk) => {
