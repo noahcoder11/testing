be39aba705292c7f5b16833e7cd3503a5396b419
admin <noahcoder11@gmail.com>
2023-01-05 14:05:47 -0800

    testing

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var a = document.querySelector("#a1")
 var a2 = document.querySelector("#a2")
 
 a.href = window.location + "/.netlify/functions/api"
-a2.href = window.location + "/.netlify/functions/api/testdata"
+a2.href = window.location + "/.netlify/functions/api/search/"
 
 console.log(window.location)
